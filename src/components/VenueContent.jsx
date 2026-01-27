2a8b4b6ead7b21057c256c1dd3b53ca8346141a5
Nightshadow855 <alexpriftis85@gmail.com>
2026-01-27 16:16:44 +0200

    added venue

--- src/components/VenueContent.jsx
+++ src/components/VenueContent.jsx
@@ -41,7 +41,7 @@ function VenueContent({ data }) {
           initial={{ scale: 1, rotate: "0deg" }}
           whileHover={{ scale: 1.1, rotate: "5deg" }}
           transition={{ duration: 0.5, ease: "easeInOut" }}
-          className={`glow-on-hover ml-2 max-w-[400px] rounded-full bg-white p-5 text-base font-bold text-black transition-colors duration-300 ease-linear`}
+          className={`glow-on-hover ml-2 max-w-[400px] cursor-pointer rounded-full bg-white p-5 text-base font-bold text-black transition-colors duration-300 ease-linear`}
         >
           {data.venueButtonTxt}
         </motion.button>
